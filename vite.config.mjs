746af1757e04550775a73d729ee06f985c2f5915
Morgana Duarte <morganaianez@gmail.com>
2024-03-09 11:23:22 -0300

    changes for deploy

--- vite.config.mjs
+++ vite.config.mjs
@@ -10,6 +10,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/search-ibge/",
   plugins: [
     Vue({
       template: { transformAssetUrls }
